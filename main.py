9fcdd51bdcb3224be46682848ec5b79a46e4b728
Abdelrahman Altamimi <tamimi6030@gmail.com>
2024-12-31 19:15:43 +0300

    add character counter

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-def read_book_content():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    return file_contents
+def main(): 
+    book_path = "books/frankenstein.txt"
+    text = read_book_content(book_path)
+    num_words = count_words(text)
+    num_chars_dict = count_characters(text)
+    print(num_chars_dict)
 
-   
+
 def count_words(text):
     words_list = text.split()
     print(len(words_list))
+
+
+def count_characters(text):
+    char_dict = {}
+    for char in text:
+        lowerd = char.lower()
+        if lowerd in char_dict:
+            char_dict[lowerd] += 1
+        else:
+            char_dict[lowerd] = 1
     
-    
-    
-def main(): 
-    count_words(read_book_content())
+    return char_dict
+
+
+def read_book_content(path):
+    with open(path) as f:
+        return f.read()
+
+   
+
     
 main()
