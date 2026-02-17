16e3cf394df1fb3866ac1c6581a3012eb82c15fb
Abdelrahman Altamimi <tamimi6030@gmail.com>
2026-02-17 20:44:35 +0300

    feat: accept a path to our book instead of using a hardcoded value

--- main.py
+++ main.py
@@ -1,54 +1,41 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = read_book_content(book_path)
-    num_words = count_words(text)
-    num_chars_dict = count_characters(text)
-    chars_sorted_list = chars_dict_to_sorted_list(num_chars_dict)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
     print_report(book_path, num_words, chars_sorted_list)
 
 
-def print_report(path, num_words, chars_sorted_list):
-    print(f"--- Begin report of {path} ---")
-    print(f"{num_words} words found in the document\n")
-    for item in chars_sorted_list:
-        char = item["char"]
-        count = item["num"]
-        print(f"The '{char}' character was found {count} times")
-    print("--- End report ---")
-
-
-def count_words(text):
-    words_list = text.split()
-    return len(words_list)
-
-
-def sort_on(d):
-    return d["num"]
-
-
-def chars_dict_to_sorted_list(num_chars_dict):
-    sorted_list = []
-    for ch in num_chars_dict:
-        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
-    sorted_list.sort(reverse=True, key=sort_on)
-    return sorted_list
-
-
-def count_characters(text):
-    char_dict = {}
-    for char in text:
-        lowered = char.lower()
-        if lowered.isalpha():
-            if lowered in char_dict:
-                char_dict[lowered] += 1
-            else:
-                char_dict[lowered] = 1
-    return char_dict
-
-
-def read_book_content(path):
+def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
